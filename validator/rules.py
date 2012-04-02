33afe5a1ddcab7b5cf02de2fafae3b5871c31956
Wilhelm Murdoch <wilhelm.murdoch@gmail.com>
2012-04-03 00:56:54 +1000

    Added quite a few more rules.

--- validator/rules.py
+++ validator/rules.py
@@ -2,7 +2,7 @@ from validator.core import Rule
 import re
 
 class Matches(Rule):
-	""" Simple rule used to determine whether one value matches another. Commonly used
+    """ Simple rule used to determine whether one value matches another. Commonly used
 	for password confirmation. """
 
 	match = ''
@@ -77,43 +77,107 @@ class Regex(Rule):
 class IsEmail(Regex):
 	def __init__(self, error = None):
 		super(IsEmail, self).__init__(r'^[a-zA-Z0-9._%-+]+@[a-zA-Z0-9._%-]+.[a-zA-Z]{2,6}$', error)
+		if not error:
+			self.error = 'This is not a valid email address.'
 
 
 
 class IsNumeric(Regex):
 	def __init__(self, error = None):
 		super(IsNumeric, self).__init__(r'^[0-9]+$', error)
+		if not error:
+			self.error = 'This is not a number.'
 
 
 
 class IsAlpha(Regex):
 	def __init__(self, error = None):
 		super(IsAlpha, self).__init__(r'^[a-zA-Z]+$', error)
+		if not error:
+			self.error = 'This is not an alpha-only string.'
 
 
 
-class IsAlNum(Regex):
+class IsAlphaNumeric(Regex):
 	def __init__(self, error = None):
-		super(IsAlNum, self).__init__(r'^[a-zA-Z0-9]+$', error)
+		super(IsAlphaNumeric, self).__init__(r'^[a-zA-Z0-9]+$', error)
+		if not error:
+			self.error = 'This is not an alpha-numeric string.'
+
+
+class IsRequired(Regex):
+	def __init__(self, error = None):
+		super(IsRequired, self).__init__(r'^.{1,}$', error)
+		if not error:
+			self.error = 'This field requires a value.'
+
+
+class IsLength(Rule):
+	length = None
+	strip = False
+	def __init__(self, length, strip = False, error = None):
+		super(IsLength, self).__init__(error)
+		self.length = int(length)
+		self.strip = bool(strip)
+
+	def run(self, field_value):
+		if len((field_value.strip() if self.strip else field_value)) != self.length:
+			if not self.error:
+				self.error = "String `%s` length does not equal `%d`" % (field_value, self.length)
+			return False
+		return True
 
 
 
-class IsRequired(Rule):
+class IsLengthBetween(Rule):
+	min = 0
+	max = 0
+	strip = False
+
+	def __init__(self, min, max, strip = False, error = None):
+		super(IsLengthBetween, self).__init__(error)
+		self.min = int(min)
+		self.max = int(max)
+		self.strip = bool(strip)
+
+	def run(self, field_value):
+		if self.min <= len((field_value.strip() if self.strip else field_value)) <= self.max:
+			return True
+		if not self.error:
+			self.error = "String `%s` length is not within `%d` and `%d`" % (field_value, self.min, self.max)
+		return False
+
+
+class IsInList(Rule):
+	list = []
+	strip = False
+	def __init__(self, list, strip = False, error = None):
+		super(IsInList, self).__init__(error)
+		self.list = list
+		self.strip = strip
+
+	def run(self, field_value):
+		if (field_value.strip() if self.strip else field_value) not in self.list:
+			if not self.error:
+				self.error = "Value of `%s` is not within the list" % field_value
+			return False
+		return True
+
+
+
+class HasKeys(Rule):
 	pass
-
-
-
-class IsWithinRange(Rule):
-	pass
-
 
 
 class IsType(Rule):
-	valid = ['bool', 'str', 'list', 'dict', 'object', 'tuple']
-	type = ''
-	def __init__(self, error = None):
-		super(IsType, self).__init__(type, error)
+	type = None
+	def __init__(self, type, error = None):
+		super(IsType, self).__init__(error)
 		self.type = type
 
 	def run(self, field_value):
-		pass
+		if not isinstance(field_value, self.type):
+			if not self.error:
+				self.error = "Type of `%s` is not type of `%s`" % (type(field_value), self.type)
+			return False
+		return True
