f0e7317895c793f48f149db5dab19ba9b61c12c6
Wilhelm Murdoch <wilhelm.murdoch@gmail.com>
2014-03-20 00:08:11 +1100

    added repo URL in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='A plain-as-vanilla validating package written for Python.',
     author='Wilhelm Murdoch',
     author_email='wilhelm.murdoch@gmail.com',
-    url='http://www.devilmayco.de/',
+    url='https://github.com/wilhelm-murdoch/validator',
     packages=find_packages(exclude=['tests', 'tests.*']),
     setup_requires=[
           'nose==1.3.1'
